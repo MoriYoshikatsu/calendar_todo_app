371a2cda6e6df5787f3dbec20a5ca56223e58939
MoriYoshikatsu <moriyosi36@gmail.com>
2025-02-05 13:13:44 +0000

    install breeze & Fullcalendar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EventController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/calendar', [EventController::class, 'show'])->name("show");
+Route::post('/calendar/create', [EventController::class, 'create'])->name("create"); // 予定の新規追加
+Route::post('/calendar/get',  [EventController::class, 'get'])->name("get"); // DBに登録した予定を取得
+Route::put('/calendar/update', [EventController::class, 'update'])->name("update"); // 予定の更新
+Route::delete('/calendar/delete', [EventController::class, 'delete'])->name("delete"); // 予定の削除
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
